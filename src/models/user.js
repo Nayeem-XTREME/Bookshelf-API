433f7f74474d0a263682319011d6cc21ac216997
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-25 00:17:37 +0600

    User registration and login

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 const userSchema = require('../schemas/user');
+const bcrypt = require('bcrypt');
+
+// Password hashing
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8); 
+    }
+    next();
+})
+
+// Finding a user by login credentials
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email});
+    if (!user) {
+        throw new Error('No user found');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Password do not match!');
+    }
+
+    return user;
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,13 +8,25 @@ router.get('/users', (req, res) => {
 })
 
 // Register a user
-router.post('/user/signup', (req, res) => {
+router.post('/user/signup', async (req, res) => {
+    const user = new User(req.body);
 
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 })
 
 // Login a user
-router.post('/user/login', (req, res) => {
-
+router.post('/user/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 })
 
 // Update a user
