1d72cc0692a93efc60ea9a3901e4950bf620baae
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-11-25 15:14:33 +0600

    Remove duplicate saving

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,7 +13,6 @@ router.post('/user/signup', async (req, res) => {
     const user = new User(req.body);
 
     try {
-        await user.save();
         const token = await user.generateAuthToken();
         res.status(201).send({user, token});
     } catch (error) {
